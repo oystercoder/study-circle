eacd272fa55d703b45ec32584de5610cc46251ff
Your Name <your-email@example.com>
2024-07-02 17:26:22 +0530

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to db')
+}).catch(()=>{
+    console.log('not connected to db')
+})
 const app=express();
 app.listen(3023,()=>{
     console.log('server is running')
