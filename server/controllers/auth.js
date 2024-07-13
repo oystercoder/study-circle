bcffaa1ab67475e3d285ad8486b42e6fd65b86e2
Your Name <your-email@example.com>
2024-07-13 11:16:22 +0530

    SIGNIN API

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import user from '../model/user.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username||!email||!password||username===""||email===""||password==="")
@@ -13,6 +14,7 @@ export const signup=async(req,res,next)=>{
        try{
 
        await newUser.save()
+      
        res.status(200).json({message:"user created successfully"})
        }catch(err)
        {
@@ -24,3 +26,38 @@ export const signup=async(req,res,next)=>{
 
 
 }
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email||!password||email===""||password==="")
+    {
+        next(errorHandler(400,"All the fields are required"))
+    }
+    try
+    {
+        const userii=await user.findOne({email:email})
+        if(!userii)
+        {
+            return next(errorHandler(400,"user not found"))
+        }
+        const isMatch=bcryptjs.compareSync(password,userii.password)
+        if(!isMatch)
+            {
+                return next(errorHandler(400,"password is incorrect"))
+            }
+            const {password:pass,...rest}=userii._doc;
+      
+        const token=jwt.sign(
+            {id:userii._id},
+            process.env.JWT_SECRET||"SECRET",
+        )
+           res.cookie("token",token,{httpOnly:true})
+           res.status(200).json({message:"user signed in successfully",user:rest})
+
+        }catch(err)
+                {
+                    next(err)
+                }
+            }
+
+    
+
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import {signup} from '../controllers/auth.js'
+import {signin} from '../controllers/auth.js'
 const route=express.Router()
 
 route.post('/signup',signup);
+route.post('/signin',signin);
 export default route
